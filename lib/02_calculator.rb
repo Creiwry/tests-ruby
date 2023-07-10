919865ac15edfab8dec25ff9559abdb769e67de4
Creiwry <jay.gardos@outlook.com>
2023-07-10 11:14:01 +0200

    make 02_calculator spec pass

---
+++ lib/02_calculator.rb
@@ -0,0 +1,38 @@
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(array)
+  sum = 0
+
+  array.each do |num|
+    sum += num
+  end
+
+  sum
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def power(num1, num2)
+  num1**num2
+end
+
+def factorial(num)
+  factorial = 1
+  if num.zero?
+    factorial = 1
+  else
+    while num.positive?
+      factorial *= num
+      num -= 1
+    end
+  end
+  factorial
+end
