5a13cd96dab3e363791822929ced5a1ce801d743
Creiwry <jay.gardos@outlook.com>
2023-07-10 21:16:23 +0200

    finish 06_pig_latin spec and make it pass

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,32 +1,32 @@
 def translate(string)
-  words = string.split(" ")
-  vowels = ['a', 'e', 'i', 'o', 'u']
+  words = string.split
   new_string_array = []
+
   words.each do |word|
-    if word.start_with?(/qu/)
-      string_array = word.chars
-      qu_group = string_array.shift(2)
-      word = string_array.join("") + qu_group.join("") +"ay"
-    elsif word.start_with?(/[^aeiou]qu/)
-      string_array = word.chars
-      qu_group = string_array.shift(3)
-      word = string_array.join("") + qu_group.join("") +"ay"
-    elsif word.start_with?(/[aeiou]/)
-      word = word + "ay"
-    elsif word.start_with?(/[^aeiou][^aeiou][^aeiou]/)
-      string_array = word.chars
-      first_three_letters = string_array.shift(3)
-      word = string_array.join("") + first_three_letters.join("") + "ay"
-    elsif word.start_with?(/[^aeiou][^aeiou]/)
-      string_array = word.chars
-      first_two_letters = string_array.shift(2)
-      word = string_array.join("") + first_two_letters.join("") + "ay"
-    else
-      string_array = word.chars
-      first_letter = string_array.shift()
-      word = string_array.join("") + first_letter + "ay"
-    end
-    new_string_array << word
+    string_array = word.chars
+    punctuation = [',', '.', '?', '!', ';', ':']
+
+    local_punctuation = string_array.pop if punctuation.include?(string_array.last)
+
+    shift_variable = get_shift_variable(word)
+    letter_group = string_array.shift(shift_variable)
+
+    new_word = "#{string_array.join}#{letter_group.join}ay#{local_punctuation}"
+    new_word = new_word.capitalize if word[0].upcase == word[0]
+    new_string_array << new_word
   end
-  new_string_array.join(" ")
+
+  new_string_array.join(' ')
+end
+
+def get_shift_variable(word)
+  if word.start_with?(/[^aeiou]qu/i) || word.start_with?(/[^aeiou][^aeiou][^aeiou]/i)
+    3
+  elsif word.start_with?(/qu/i) || word.start_with?(/[^aeiou][^aeiou]/i)
+    2
+  elsif word.start_with?(/[aeiou]/i)
+    0
+  else
+    1
+  end
 end
--- spec/06_pig_latin_spec.rb
+++ spec/06_pig_latin_spec.rb
@@ -46,6 +46,15 @@ describe "#translate" do
     expect(s).to eq("ethay ickquay ownbray oxfay")
   end
 
+  it "keeps capital letters capitalized" do
+    s = translate("the Quick Brown Fox")
+    expect(s).to eq("ethay Ickquay Ownbray Oxfay")
+  end
+
+  it "keeps original punctuation" do
+    s = translate("Patrick listened, Spongebob played.")
+    expect(s).to eq("Atrickpay istenedlay, Ongebobspay ayedplay.")
+  end
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
